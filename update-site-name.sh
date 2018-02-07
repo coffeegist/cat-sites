b1e0371251bdc34c6c5076c997d126e3e70a5a36
Adam Brown <audrummer15@gmail.com>
2018-02-07 13:54:00 -0500

    * Fix update command.

--- update-site-name.sh
+++ update-site-name.sh
@@ -3,5 +3,5 @@
 read -r -p "What is your sites url? (ex: https://google.com) " site_url
 read -r -p "What is your sites title? " site_title
 
-grep -rl "\[cat-site-name\]" * -R | xargs sed -i 's/\[cat-site-name\]/$site_title/g'
-grep -rl "\[cat-site-url\]" * -R | xargs sed -i 's/\[cat-site-name\]/$site_title/g'
+grep -rl "\[cat-site-name\]" * -R | xargs sed -i "s/\[cat-site-name\]/$site_title/g"
+grep -rl "\[cat-site-url\]" * -R | xargs sed -i "s/\[cat-site-name\]/$site_title/g"
