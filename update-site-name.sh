f38a856c2a967de729e007334d98befd8d32868a
Adam Brown <audrummer15@gmail.com>
2018-02-07 13:43:40 -0500

    * Add script to update site details.

---
+++ update-site-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -r -p "What is your sites url? (ex: https://google.com) " site_url
+read -r -p "What is your sites title? " site_title
+
+grep -rl "\[cat-site-name\]" * -R | xargs sed -i 's/\[cat-site-name\]/$site_title/g'
+grep -rl "\[cat-site-url\]" * -R | xargs sed -i 's/\[cat-site-name\]/$site_title/g'
